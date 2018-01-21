ec83b82b7e3cbeef9cc0ed7b730e0d481a5a41fd
Shudd3r <q3.shudder@gmail.com>
2018-01-21 05:11:41 +0100

    Psr-7 Message: implemented message methods (unsafe headers)

--- src/Message/MessageMethods.php
+++ src/Message/MessageMethods.php
@@ -3,19 +3,107 @@
 namespace Shudd3r\Http\Src\Message;
 
 use Psr\Http\Message\StreamInterface;
+use InvalidArgumentException;
 
 
 trait MessageMethods
 {
-    public function getProtocolVersion() {}
-    public function withProtocolVersion($version) {}
-    public function getHeaders() {}
-    public function hasHeader($name) {}
-    public function getHeader($name) {}
-    public function getHeaderLine($name) {}
-    public function withHeader($name, $value) {}
-    public function withAddedHeader($name, $value) {}
-    public function withoutHeader($name) {}
-    public function getBody() {}
-    public function withBody(StreamInterface $body) {}
+    private $body;
+    private $version;
+    private $headers;
+
+    private $supportedProtocolVersions = ['1.0', '1.1', '2'];
+    private $headerNames = [];
+
+    public function getProtocolVersion() {
+        return $this->version;
+    }
+
+    public function withProtocolVersion($version) {
+        $clone = clone $this;
+        $clone->version = $this->validProtocolVersion($version);
+        return $clone;
+    }
+
+    public function getHeaders() {
+        return $this->headers;
+    }
+
+    public function hasHeader($name) {
+        return is_string($name) && isset($this->headerNames[strtolower($name)]);
+    }
+
+    public function getHeader($name) {
+        return $this->hasHeader($name) ? $this->headers[$this->headerNames[strtolower($name)]] : [];
+    }
+
+    public function getHeaderLine($name) {
+        $header = $this->getHeader($name);
+        return empty($header) ? '' : implode(', ', $header);
+    }
+
+    public function withHeader($name, $value) {
+        $clone = clone $this;
+        $clone->setHeader($name, $value);
+        return $clone;
+    }
+
+    public function withAddedHeader($name, $value) {
+        if (!$this->hasHeader($name)) { return $this->withHeader($name, $value); }
+
+        $name  = $this->headerNames[strtolower($name)];
+        $value = $this->headerValue($value);
+
+        $clone = clone $this;
+        $clone->headers[$name] = array_merge($clone->headers[$name], $value);
+        return $clone;
+    }
+
+    public function withoutHeader($name) {
+        $clone = clone $this;
+        $clone->removeHeader(strtolower($name));
+        return $clone;
+    }
+
+    public function getBody(): StreamInterface {
+        return $this->body;
+    }
+
+    public function withBody(StreamInterface $body) {
+        $clone = clone $this;
+        $clone->body = $body;
+        return $clone;
+    }
+
+    private function loadHeaders(array $headers) {
+        foreach ($headers as $name => $value) {
+            $this->setHeader($name, $value);
+        }
+    }
+
+    private function setHeader($name, $value) {
+        $this->headers[$name] = $this->headerValue($value);
+        $this->headerNames[strtolower($name)] = $name;
+    }
+
+    private function headerValue($value) {
+        return is_string($value) ? [$value] : $value;
+    }
+
+    private function removeHeader($header_id) {
+        if (!isset($this->headerNames[$header_id])) { return; }
+        unset($this->headers[$this->headerNames[$header_id]]);
+        unset($this->headerNames[$header_id]);
+    }
+
+    private function validProtocolVersion($version) {
+        if (!is_string($version)) {
+            throw new InvalidArgumentException('Invalid HTTP protocol version type - expected string');
+        }
+
+        if (!in_array($version, $this->supportedProtocolVersions)) {
+            throw new InvalidArgumentException('Unsupported HTTP protocol version - expected <' . implode('|', $this->supportedProtocolVersions) . '> string.');
+        }
+        return $version;
+    }
 }
---
+++ tests/Message/MessageMethodsTest.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Shudd3r\Http\Tests\Message;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\MessageInterface;
+use Shudd3r\Http\Tests\Doubles\DummyStream;
+use Shudd3r\Http\Tests\Doubles\MessageMethodsShell;
+use InvalidArgumentException;
+
+
+class MessageMethodsTest extends TestCase
+{
+    private function message(array $headers = [], $version = null) {
+        if (!$version) { return new MessageMethodsShell(new DummyStream(), $headers); }
+        return new MessageMethodsShell(new DummyStream(), $headers, $version);
+    }
+
+    public function testInstantiatingMessage() {
+        $this->assertInstanceOf(MessageInterface::class, $this->message());
+    }
+
+    public function testSetSupportedProtocolVersion() {
+        $this->assertSame('2', $this->message([], '2')->getProtocolVersion());
+        $this->assertSame('1.0', $this->message()->withProtocolVersion('1.0')->getProtocolVersion());
+        $this->assertSame('1.1', $this->message()->withProtocolVersion('1.1')->getProtocolVersion());
+        $this->assertSame('2', $this->message()->withProtocolVersion('2')->getProtocolVersion());
+    }
+
+    public function testProtocolVersionChange_ReturnsNewObject() {
+        $original = $this->message();
+        $modified = $original->withProtocolVersion('2');
+        $this->assertEquals($modified, $original->withProtocolVersion('2'));
+        $this->assertNotSame($modified, $original->withProtocolVersion('2'));
+    }
+
+    public function testInstantiateWithUnsupportedProtocolVersion_ThrowsException() {
+        $this->expectException(InvalidArgumentException::class);
+        $this->message([], '0.9');
+    }
+
+    public function testInstantiateWithInvalidProtocolVersionType_ThrowsException() {
+        $this->expectException(InvalidArgumentException::class);
+        $this->message([], 1.1);
+    }
+
+    public function testSetUnsupportedProtocolVersion_ThrowsException() {
+        $message = $this->message();
+        $this->expectException(InvalidArgumentException::class);
+        $message->withProtocolVersion('0.9');
+    }
+
+    public function testSetInvalidProtocolVersionType_ThrowsException() {
+        $message = $this->message();
+        $this->expectException(InvalidArgumentException::class);
+        $message->withProtocolVersion(1.1);
+    }
+
+    public function testGetBody_ReturnsPassedStream() {
+        $body = new DummyStream();
+        $this->assertSame($body, $this->message()->withBody($body)->getBody());
+        $message = new MessageMethodsShell($body, []);
+        $this->assertSame($body, $message->getBody());
+    }
+
+    public function testWithBody_ReturnsNewObject() {
+        $original = $this->message();
+        $modified = $original->withBody(new DummyStream());
+        $this->assertEquals($original, $modified);
+        $this->assertNotSame($original, $modified);
+    }
+
+    public function testHasHeaderForUnknownHeaderName_ReturnsFalse() {
+        $this->assertFalse($this->message()->hasHeader('test'));
+    }
+
+    public function testHasHeaderForKnownHeaderName_ReturnsTrue() {
+        $this->assertTrue($this->message(['test' => 'header string'])->hasHeader('test'));
+    }
+
+    public function testGetHeaderForValuePassedAsArray_ReturnsSameArray() {
+        $value = ['header value'];
+        $this->assertSame($value, $this->message()->withHeader('test', $value)->getHeader('test'));
+        $this->assertSame($value, $this->message(['test' => $value])->getHeader('test'));
+    }
+
+    public function testGetHeaderLine_ReturnsCommaConcatenatedHeaderString() {
+        $message = $this->message(['TwoValues' => ['first value', 'second value']]);
+        $this->assertSame('first value, second value', $message->getHeaderLine('TwoValues'));
+    }
+
+    public function testWithHeader_ReturnsNewObject() {
+        $original = $this->message(['test' => ['old value']]);
+        $modified = $original->withHeader('test', ['new value']);
+        $this->assertEquals(['old value'], $original->getHeader('test'));
+        $this->assertEquals($original, $modified->withHeader('test', ['old value']));
+        $this->assertNotSame($original, $modified->withHeader('test', ['old value']));
+    }
+
+    public function testGetHeaderForValuePassedAsString_ReturnsArrayWithSameString() {
+        $this->assertSame(['string'], $this->message()->withHeader('test', 'string')->getHeader('test'));
+    }
+
+    public function testWithAddedHeader_AppendsValueToExistingValues() {
+        $message = $this->message()->withHeader('test', 'first');
+        $changed = $message->withAddedHeader('test', 'second');
+        $this->assertSame(['first', 'second'], $changed->getHeader('test'));
+        $this->assertNotSame($changed, $message->withAddedHeader('test', 'second'));
+    }
+
+    public function testWithoutHeader_RemovesHeader() {
+        $this->assertFalse($this->message(['test' => 'value'])->withoutHeader('test')->hasHeader('test'));
+    }
+
+    public function testWithoutHeader_ReturnsNewObject() {
+        $original = $this->message();
+        $modified = $original->withoutHeader('doesntMatter');
+        $this->assertEquals($original, $modified);
+        $this->assertNotSame($original, $modified);
+    }
+
+    public function testHasHeader_HeaderNameIsNotCaseSensitive() {
+        $message = $this->message(['tEsT' => ['string']]);
+        $this->assertTrue($message->hasHeader('TeSt'));
+    }
+
+    public function testGetHeader_HeaderNameIsNotCaseSensitive() {
+        $message = $this->message(['TEST' => ['string']]);
+        $this->assertSame(['string'], $message->getHeader('teST'));
+    }
+
+    public function testGetHeaderLine_HeaderNameIsNotCaseSensitive() {
+        $message = $this->message(['TESTlowercase' => ['first', 'SECOND']]);
+        $this->assertSame('first, SECOND', $message->getHeaderLine('testLOWERCASE'));
+    }
+
+    public function testWithHeader_HeaderNameIsNotCaseSensitive() {
+        $message = $this->message(['TEst' => ['old value']]);
+        $this->assertSame(['new value'], $message->withHeader('teST', ['new value'])->getHeader('tESt'));
+    }
+
+    public function testWithAddedHeader_HeaderNameIsNotCaseSensitive() {
+        $message = $this->message(['TEst' => ['old value']]);
+        $this->assertSame(['old value', 'new value'], $message->withAddedHeader('teST', ['new value'])->getHeader('tESt'));
+    }
+
+    public function testWithAddedHeader_OriginalKeysArePreserved() {
+        $message = $this->message(['TEst' => ['old value']]);
+        $this->assertSame(['TEst' => ['old value', 'new value']], $message->withAddedHeader('teST', ['new value'])->getHeaders());
+    }
+
+    public function testWithoutHeader_HeaderNamesIsNotCaseSensitive() {
+        $message = $this->message(['garbage' => ['one', 'two'], 'useful' => 'three']);
+        $this->assertFalse($message->withoutHeader('GARBAGE')->hasHeader('garbage'));
+        $this->assertSame(['useful' => ['three']], $message->withoutHeader('GARBAGE')->getHeaders());
+    }
+
+    public function testGetHeaders_ReturnsHeaderNamesWithOriginalCase() {
+        $this->assertSame(['tEsT' => ['value']], $this->message()->withHeader('tEsT', 'value')->getHeaders());
+        $this->assertSame(['tEsT' => ['value']], $this->message(['tEsT' => 'value'])->getHeaders());
+    }
+}
