573e93a298712f836e01208d0efea813cb80def6
Shudd3r <q3.shudder@gmail.com>
2018-01-23 22:19:00 +0100

    Psr-7 ServerRequest: basic implementation (unsafe)

--- src/Message/ServerRequest.php
+++ src/Message/ServerRequest.php
@@ -11,6 +11,13 @@ class ServerRequest implements ServerRequestInterface
 {
     use RequestMethods;
 
+    private $server;
+    private $cookie;
+    private $query;
+    private $attributes;
+    private $parsedBody;
+    private $files;
+
     public function __construct(
         string $method,
         UriInterface $uri,
@@ -23,21 +30,76 @@ class ServerRequest implements ServerRequestInterface
         $this->body = $body;
         $this->version = isset($params['version']) ? $this->validProtocolVersion($params['version']) : '1.1';
         $this->target = isset($params['target']) ? $this->validRequestTarget($params['target']) : null;
+        $this->server = isset($params['server']) ? (array) $params['server'] : [];
+        $this->cookie = isset($params['cookie']) ? (array) $params['cookie'] : [];
+        $this->query = isset($params['query']) ? (array) $params['query'] : [];
+        $this->attributes = isset($params['attributes']) ? (array) $params['attributes'] : [];
+        $this->parsedBody = isset($params['parsedBody']) ? (array) $params['parsedBody'] : [];
+        $this->files = isset($params['files']) ? (array) $params['files'] : [];
         $this->loadHeaders($headers);
         $this->resolveHostHeader();
     }
 
-    public function getServerParams() {}
-    public function getCookieParams() {}
-    public function withCookieParams(array $cookies) {}
-    public function getQueryParams() {}
-    public function withQueryParams(array $query) {}
-    public function getUploadedFiles() {}
-    public function withUploadedFiles(array $uploadedFiles) {}
-    public function getParsedBody() {}
-    public function withParsedBody($data) {}
-    public function getAttributes() {}
-    public function getAttribute($name, $default = null) {}
-    public function withAttribute($name, $value) {}
-    public function withoutAttribute($name) {}
+    public function getServerParams(): array {
+        return $this->server;
+    }
+
+    public function getCookieParams() {
+        return $this->cookie;
+    }
+    public function withCookieParams(array $cookies) {
+        $clone = clone $this;
+        $clone->cookie = $cookies;
+        return $clone;
+    }
+
+    public function getQueryParams() {
+        return $this->query;
+    }
+
+    public function withQueryParams(array $query) {
+        $clone = clone $this;
+        $clone->query = $query;
+        return $clone;
+    }
+
+    public function getUploadedFiles() {
+        return $this->files;
+    }
+
+    public function withUploadedFiles(array $uploadedFiles) {
+        $clone = clone $this;
+        $clone->files = $uploadedFiles;
+        return $clone;
+    }
+
+    public function getParsedBody() {
+        return $this->parsedBody;
+    }
+
+    public function withParsedBody($data) {
+        $clone = clone $this;
+        $clone->parsedBody = $data;
+        return $clone;
+    }
+
+    public function getAttributes() {
+        return $this->attributes;
+    }
+
+    public function getAttribute($name, $default = null) {
+        return array_key_exists($name, $this->attributes) ? $this->attributes[$name] : $default;
+    }
+
+    public function withAttribute($name, $value) {
+        $clone = clone $this;
+        $clone->attributes[$name] = $value;
+        return $clone;
+    }
+
+    public function withoutAttribute($name) {
+        $clone = clone $this;
+        unset($clone->attributes[$name]);
+        return $clone;
+    }
 }
--- tests/Message/ServerRequestTest.php
+++ tests/Message/ServerRequestTest.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Shudd3r\Http\Src\Message\ServerRequest;
 use PHPUnit\Framework\TestCase;
 use Shudd3r\Http\Tests\Doubles\DummyStream;
+use Shudd3r\Http\Tests\Doubles\FakeUploadedFile;
 use Shudd3r\Http\Tests\Doubles\FakeUri;
 
 
@@ -17,5 +18,80 @@ class ServerRequestTest extends TestCase
 
     public function testInstantiation() {
         $this->assertInstanceOf(ServerRequestInterface::class, $this->request());
+    }
+
+    public function testGetServerParams_ReturnsInstanceServerParamsArray() {
+        $params = ['key' => 'value'];
+        $this->assertSame($params, $this->request(['server' => $params])->getServerParams());
+    }
+
+    /**
+     * @dataProvider instanceProperties
+     * @param $method
+     * @param $key
+     * @param $params
+     */
+    public function testGetters_ReturnConstructorProperties($method, $params, $key) {
+        $this->assertSame($params, $this->request([$key => $params])->$method());
+    }
+
+    public function instanceProperties() {
+        return [
+            'cookie' => ['getCookieParams', ['key' => 'value'], 'cookie'],
+            'query' => ['getQueryParams', ['key' => 'value'], 'query'],
+            'attrib' => ['getAttributes', ['key' => 'value'], 'attributes'],
+            'pBody' => ['getParsedBody', ['key' => 'value'], 'parsedBody'],
+            'files' => ['getUploadedFiles', ['key' => new FakeUploadedFile()], 'files']
+        ];
+    }
+
+    public function testGetAttribute_ReturnsSpecifiedAttributeValue() {
+        $request = $this->request(['attributes' => ['name' => 'value']]);
+        $this->assertSame('value', $request->getAttribute('name', 'default'));
+        $request = $this->request(['attributes' => ['name' => null]]);
+        $this->assertSame(null, $request->getAttribute('name', 'default'));
+    }
+
+    public function testGetAttribute_ReturnsDefaultValueIfAttributeNotPresent() {
+        $request = $this->request(['attributes' => ['unknownName' => 'value']]);
+        $this->assertSame('default', $request->getAttribute('name', 'default'));
+        $this->assertSame(null, $request->getAttribute('name'));
+    }
+
+    /**
+     * @dataProvider mutatorMethods
+     * @param $method
+     * @param $params
+     */
+    public function testMutatorMethods_ReturnNewInstance($method, $params) {
+        $original = $this->request();
+        $derived1 = $original->$method($params);
+        $derived2 = $original->$method($params);
+        $this->assertEquals($derived1, $derived2);
+        $this->assertNotSame($derived1, $derived2);
+    }
+
+    public function mutatorMethods() {
+        return [
+            'cookie' => ['withCookieParams', ['key' => 'value']],
+            'query' => ['withQueryParams', ['key' => 'value']],
+            'pBody' => ['withParsedBody', ['key' => 'value']],
+            'files' => ['withUploadedFiles', ['key' => new FakeUploadedFile()]]
+        ];
+    }
+
+    public function testAttributeMutation_ReturnsNewInstance() {
+        $original = $this->request();
+        list($name, $value) = ['name', 'value'];
+        $derived1 = $original->withAttribute($name, $value);
+        $derived2 = $original->withAttribute($name, $value);
+        $this->assertEquals($derived1, $derived2);
+        $this->assertNotSame($derived1, $derived2);
+
+        $original = $derived1;
+        $derived1 = $original->withoutAttribute($name);
+        $derived2 = $original->withoutAttribute($name);
+        $this->assertEquals($derived1, $derived2);
+        $this->assertNotSame($derived1, $derived2);
     }
 }
