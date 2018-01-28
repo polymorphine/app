244b1aa074e3dcc2b845e18382e4f369399d88ad
Shudd3r <q3.shudder@gmail.com>
2018-01-28 04:59:00 +0100

    App: basic Psr-11 Container integration Configuration entries are encapsulated within App. Once values are set they're not available from global scope. Container is immutable from outside, but defined closures still have access to underlying registry and its setter methods. It's dirty, but might be useful - need to think it over.

--- src/App.php
+++ src/App.php
@@ -4,12 +4,25 @@ namespace Shudd3r\Http\Src;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Shudd3r\Http\Src\Container\RegistryInput;
+use Shudd3r\Http\Src\Container\Registry;
 use Shudd3r\Http\Tests\Doubles\DummyResponse;
+use Shudd3r\Http\Src\Container\Container;
 
 
 class App
 {
+    private $registry;
+
+    public function __construct(Registry $registry = null) {
+        $this->registry = $registry ?? new Registry();
+    }
+
     public function execute(ServerRequestInterface $request): ResponseInterface {
-        return new DummyResponse();
+        return new DummyResponse(new Container($this->registry));
+    }
+
+    public function config(string $id): RegistryInput {
+        return $this->registry->entry($id);
     }
 }
--- tests/AppTest.php
+++ tests/AppTest.php
@@ -1,17 +1,54 @@
 <?php
 
-namespace Shudd3r\Http\Src;
+namespace Shudd3r\Http\Tests;
 
-
+use Psr\Container\NotFoundExceptionInterface;
+use Shudd3r\Http\Src\App;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Shudd3r\Http\Src\Container\Registry;
 use Shudd3r\Http\Tests\Doubles\DummyRequest;
-use Shudd3r\Http\Tests\Doubles\DummyResponse;
+
 
 class AppTest extends TestCase
 {
+    private function app(Registry $registry = null) {
+        return $registry ? new App($registry) : new App();
+    }
+
     public function testExecuteReturnsDummy() {
-        $app = new App();
-        $response = $app->execute(new DummyRequest());
-        $this->assertEquals($response, new DummyResponse());
+        $app = $this->app();
+        $this->assertInstanceOf(ResponseInterface::class, $app->execute(new DummyRequest()));
+    }
+
+    public function testConfigValueIsSetInRegistry() {
+        $app = $this->app();
+        $app->config('test')->value('Hello World!');
+        $app->config('lazy')->lazy(function () {
+            return 'Lazy Foo';
+        });
+        $app->config('bar')->lazy(function () {
+            return substr($this->get('test'), 0, 6) . $this->get('lazy') . '!';
+        });
+
+        $container = $app->execute(new DummyRequest())->container;
+
+        $this->assertTrue($container->has('test') && $container->has('lazy') && $container->has('bar'));
+        $this->assertSame('Hello World!', $container->get('test'));
+        $this->assertSame('Lazy Foo', $container->get('lazy'));
+        $this->assertSame('Hello Lazy Foo!', $container->get('bar'));
+    }
+
+    public function testInvalidContainerIdType_ThrowsException() {
+        $container = $this->app()->execute(new DummyRequest())->container;
+        $this->expectException(ContainerExceptionInterface::class);
+        $container->has(23);
+    }
+
+    public function testAccessingAbsentIdFromContainer_ThrowsException() {
+        $container = $this->app()->execute(new DummyRequest())->container;
+        $this->expectException(NotFoundExceptionInterface::class);
+        $container->get('not.set');
     }
 }
