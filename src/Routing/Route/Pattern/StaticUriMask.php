b9c2fef83112c8962cb702166adbe9217efe2e3a
Shudd3r <q3.shudder@gmail.com>
2018-03-22 04:32:48 +0100

    Extracted query pattern as StaticUriMask dependency

--- src/Routing/Route/Pattern/StaticUriMask.php
+++ src/Routing/Route/Pattern/StaticUriMask.php
@@ -34,7 +34,31 @@ class StaticUriMask implements Pattern
 
     public function matchedRequest(ServerRequestInterface $request): ?ServerRequestInterface
     {
-        return $this->compareUri($request->getUri()) ? $request : null;
+        $uri = $request->getUri();
+
+        $scheme = $this->uri->getScheme();
+        if ($scheme && $scheme !== $uri->getScheme()) {
+            return null;
+        }
+
+        $auth = $this->uri->getAuthority();
+        if ($auth && $auth !== $uri->getAuthority()) {
+            return null;
+        }
+
+        $path = $this->uri->getPath();
+        if ($path && $uriPath = $uri->getPath()) {
+            if (($path[0] === '/' && $path !== $uriPath) || ($path[0] !== '/' && !strpos($uriPath, $path))) {
+                return null;
+            }
+        }
+
+        if ($query = $this->uri->getQuery()) {
+            $pattern = new StaticQueryPattern($query);
+            return $pattern->matchedRequest($request);
+        }
+
+        return $request;
     }
 
     public function uri(array $params, UriInterface $prototype): UriInterface
@@ -74,37 +98,11 @@ class StaticUriMask implements Pattern
         }
 
         if ($query = $this->uri->getQuery()) {
-            $prototype = $prototype->withQuery($this->combinedQuery($query, $prototype->getQuery()));
+            $pattern = new StaticQueryPattern($query);
+            $prototype = $pattern->uri($params, $prototype);
         }
 
         return $prototype;
-    }
-
-    private function compareUri(UriInterface $uri)
-    {
-        $scheme = $this->uri->getScheme();
-        if ($scheme && $scheme !== $uri->getScheme()) {
-            return false;
-        }
-
-        $auth = $this->uri->getAuthority();
-        if ($auth && $auth !== $uri->getAuthority()) {
-            return false;
-        }
-
-        $path = $this->uri->getPath();
-        if ($path && $uriPath = $uri->getPath()) {
-            if (($path[0] === '/' && $path !== $uriPath) || ($path[0] !== '/' && !strpos($uriPath, $path))) {
-                return false;
-            }
-        }
-
-        $query = $this->uri->getQuery();
-        if ($query && !$this->queryMatch($query, $uri->getQuery())) {
-            return false;
-        }
-
-        return true;
     }
 
     private function checkConflict(string $routeSegment, string $prototypeSegment)
@@ -113,72 +111,5 @@ class StaticUriMask implements Pattern
             $message = 'Uri conflict in `%s` prototype segment for `%s` uri';
             throw new UnreachableEndpointException(sprintf($message, $prototypeSegment, (string) $this->uri));
         }
-    }
-
-    private function combinedQuery(string $routeQuery, string $prototypeQuery)
-    {
-        if (empty($prototypeQuery)) {
-            return $routeQuery;
-        }
-
-        $requiredSegments = $this->queryValues($routeQuery);
-        $prototypeSegments = $this->queryValues($prototypeQuery);
-
-        foreach ($requiredSegments as $name => $value) {
-            if (isset($value) && isset($prototypeSegments[$name]) && $prototypeSegments[$name] !== $value) {
-                $message = 'Query param conflict for `%s` segment in `%s` uri';
-                throw new UnreachableEndpointException(sprintf($message, $name, (string) $this->uri));
-            }
-
-            if (!isset($value) && isset($prototypeSegments[$name])) {
-                continue;
-            }
-
-            $prototypeSegments[$name] = $value;
-        }
-
-        $query = [];
-        foreach ($prototypeSegments as $name => $value) {
-            $query[] = isset($value) ? $name . '=' . $value : $name;
-        }
-
-        return implode('&', $query);
-    }
-
-    private function queryMatch($routeQuery, $requestQuery)
-    {
-        if (empty($requestQuery)) { return false; }
-
-        $requiredSegments = $this->queryValues($routeQuery);
-        $requestSegments = $this->queryValues($requestQuery);
-
-        foreach ($requiredSegments as $key => $value) {
-            if (!isset($requestSegments[$key])) {
-                return false;
-            }
-
-            if (!isset($value)) {
-                continue;
-            }
-
-            if ($value !== $requestSegments[$key]) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private function queryValues(string $query): array
-    {
-        $segments = explode('&', $query);
-
-        $segmentValues = [];
-        foreach ($segments as $segment) {
-            [$name, $value] = explode('=', $segment) + [false, null];
-            $segmentValues[$name] = $value;
-        }
-
-        return $segmentValues;
     }
 }
