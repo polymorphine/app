6bb9a1bbcbb2c8ceaf32876d49194b64b1429351
Shudd3r <q3.shudder@gmail.com>
2018-05-10 03:07:54 +0200

    Added middleware setting response headers

---
+++ src/Server/Middleware/SetResponseHeaders.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Http package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Http\Server\Middleware;
+
+use Psr\Http\Server\MiddlewareInterface;
+use Polymorphine\Http\Message\Response\Headers\ResponseHeadersCollection;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+
+class SetResponseHeaders implements MiddlewareInterface
+{
+    private $headers;
+
+    public function __construct(ResponseHeadersCollection $cookies)
+    {
+        $this->headers = $cookies;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        return $this->headers->setHeaders($response);
+    }
+}
---
+++ tests/Server/Middleware/SetResponseHeadersTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Http package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Http\Tests\Server\Middleware;
+
+use PHPUnit\Framework\TestCase;
+use Polymorphine\Http\Message\Response\Headers\ResponseHeadersCollection;
+use Polymorphine\Http\Server\Middleware\SetResponseHeaders;
+use Polymorphine\Http\Tests\Doubles\FakeRequestHandler;
+use Polymorphine\Http\Tests\Doubles\FakeResponse;
+use Polymorphine\Http\Tests\Doubles\FakeServerRequest;
+
+
+class SetResponseHeadersTest extends TestCase
+{
+    public function testProcessing()
+    {
+        $headers = [
+            'Set-Cookie' => [
+                'fooCookie=foo; Expires=Tuesday, 01-May-2018 01:00:00 UTC; MaxAge=3600; Secure; HttpOnly',
+                'barCookie=; Expires=Thursday, 02-May-2013 00:00:00 UTC; MaxAge=-157680000'
+            ]
+        ];
+
+        $middleware = new SetResponseHeaders(new ResponseHeadersCollection($headers));
+        $handler = new FakeRequestHandler(new FakeResponse('test'));
+        $response = $middleware->process(new FakeServerRequest(), $handler);
+
+        $this->assertSame('test', (string) $response->getBody());
+
+        $this->assertSame($headers, $response->getHeaders());
+    }
+}
